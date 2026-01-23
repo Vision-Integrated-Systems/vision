f5fa6497a4d1c995a06420d6278b10e8d46f7798
Jeff Hogg <67076205+jeffreyehogg@users.noreply.github.com>
2026-01-23 13:56:32 -0600

    add logos folder

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
             <Link href="/" className="shrink-0 relative z-50">
               <div className="relative h-8 w-40">
                 <Image
-                  src="/vision-logo.png"
+                  src="/logos/vision-logo.png"
                   alt="Vision Integrated Systems"
                   fill
                   sizes="160px"
@@ -63,7 +63,7 @@ export default function Header() {
                   priority
                 />
                 <Image
-                  src="/vision-logo-white.png"
+                  src="/logos/vision-logo-white.png"
                   alt="Vision Integrated Systems"
                   fill
                   sizes="160px"
--- components/StructuredData.tsx
+++ components/StructuredData.tsx
@@ -2,34 +2,28 @@ export default function StructuredData() {
   const jsonLd = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
-    "name": "Vision Integrated Systems",
-    "image": "https://vision.jeffhogg.com/vision-logo.png",
-    "telephone": "832-535-1991",
-    "email": "info@vision-texas.com",
-    "address": {
+    name: "Vision Integrated Systems",
+    image: "https://vision.jeffhogg.com/logos/vision-logo.png",
+    telephone: "832-535-1991",
+    email: "info@vision-texas.com",
+    address: {
       "@type": "PostalAddress",
-      "streetAddress": "32311 Tamina Rd, Suite A",
-      "addressLocality": "Magnolia",
-      "addressRegion": "TX",
-      "postalCode": "77354",
-      "addressCountry": "US"
+      streetAddress: "32311 Tamina Rd, Suite A",
+      addressLocality: "Magnolia",
+      addressRegion: "TX",
+      postalCode: "77354",
+      addressCountry: "US",
     },
-    "url": "https://vision-texas.com",
-    "openingHoursSpecification": [
+    url: "https://vision-texas.com",
+    openingHoursSpecification: [
       {
         "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday"
-        ],
-        "opens": "08:00",
-        "closes": "17:00"
-      }
+        dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+        opens: "08:00",
+        closes: "17:00",
+      },
     ],
-    "priceRange": "$$"
+    priceRange: "$$",
   };
 
   return (
